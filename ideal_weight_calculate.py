a2be368be88ec59df185858e7f4668d79eb4d2bf
Mehran-SP <mehran.safaripour@gmail.com>
2019-08-21 22:47:46 +0430

    Add some comment

--- ideal_weight_calculate.py
+++ ideal_weight_calculate.py
@@ -1,13 +1,24 @@
 from colorama import Fore,Style,init
-init(autoreset=True)
+init(autoreset=True) # for reset the color in new element
 
 def IBW():
+    """
+        calculate the ideal body weight base on this formola :
+        Male IBW => 50 kg + 2.3 kg * (height(inch) - 60)
+        Female IBW => 45.5 kg + 2.3 kg * (height(inch) - 60)
+    """
     pass
 
 def BMI():
+    """
+        Body Mass Index is a standard to determine your
+        health situation. the formola is :
+        BMI => weight (kg) / height (m) * height (m)
+    """
     pass
 
 if __name__ == "__main__":
     print(Fore.LIGHTGREEN_EX+"Welcome to ideal body calculator!"+"\n"+"Please answer these questions :")
     gender = input("What is your gender ? ( Male(m) / Female(m) ) : ")
-    height =float(input("how much do you height ? (cm) "))
+    height = float(input("How much do you height ? (cm) :"))
+    weight = int(input("How much do you weight ? (Kg) :"))
