4e4c9d124bf7bad4de525995497a57110583fdc5
Mehran-SP <mehran.safaripour@gmail.com>
2019-08-21 21:19:37 +0430

    Add Base Skeleton of app

--- ideal_weight_calculate.py
+++ ideal_weight_calculate.py
@@ -1,2 +1,13 @@
+from colorama import Fore,Style,init
+init(autoreset=True)
 
+def IBW():
+    pass
 
+def BMI():
+    pass
+
+if __name__ == "__main__":
+    print(Fore.LIGHTGREEN_EX+"Welcome to ideal body calculator!"+"\n"+"Please answer these questions :")
+    gender = input("What is your gender ? ( Male(m) / Female(m) ) : ")
+    height =float(input("how much do you height ? (cm) "))
