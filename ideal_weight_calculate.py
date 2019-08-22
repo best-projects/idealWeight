6ffb18f469d482d912630987992e4f8b6176e7f1
arezoo <arezoo.darvishi69@yahoo.com>
2019-08-22 14:10:36 +0430

    calculate bmi in bmi method

--- ideal_weight_calculate.py
+++ ideal_weight_calculate.py
@@ -1,7 +1,9 @@
-from colorama import Fore,Style,init
-init(autoreset=True) # for reset the color in new element
+from colorama import Fore, Style, init
+import traceback
+init(autoreset=True)  # for reset the color in new element
 
-def IBW():
+
+def ibw():
     """
         calculate the ideal body weight base on this formola :
         Male IBW => 50 kg + 2.3 kg * (height(inch) - 60)
@@ -9,16 +11,34 @@ def IBW():
     """
     pass
 
-def BMI():
+
+def bmi(weight, height):
     """
         Body Mass Index is a standard to determine your
         health situation. the formola is :
         BMI => weight (kg) / height (m) * height (m)
     """
-    pass
+    try:
+        type = ''
+        bmi = weight / (height * height)
+        print(bmi)
+        if bmi < 19:
+            type = "کمبود وزن"
+        elif bmi >= 19 and bmi < 25:
+            type = "مطلوب"
+        elif bmi >= 25 and bmi < 30:
+            type = "اضافه وزن"
+        elif bmi >= 30:
+            type = "چاق"
+        return type
+    except:
+        print(traceback.format_exc())
+        return []
+
 
 if __name__ == "__main__":
-    print(Fore.LIGHTGREEN_EX+"Welcome to ideal body calculator!"+"\n"+"Please answer these questions :")
+    print(Fore.LIGHTGREEN_EX + "Welcome to ideal body calculator!" + "\n" + "Please answer these questions :")
     gender = input("What is your gender ? ( Male(m) / Female(m) ) : ")
     height = float(input("How much do you height ? (cm) :"))
     weight = int(input("How much do you weight ? (Kg) :"))
+    bmi = print("شرایط بدنی : " + str(bmi(weight,( height / 100))))
